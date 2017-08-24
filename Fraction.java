cc6de278ea587b8e6320633461c5c3297d5d96a6
Sami Rollins <srollins@cs.usfca.edu>
2017-08-23 18:30:46 -0700

    add comments

--- Fraction.java
+++ Fraction.java
@@ -1,15 +1,25 @@
-
+/**
+	* A class to represent a fraction.
+	* @author srollins
+**/
 public class Fraction {
 
+	//data members
 	private int numerator;
 	private int denominator;
 
+	/**
+		* Constructor.
+	**/
 	public Fraction(int numerator, int denominator) {
 		this.numerator = numerator;
 		this.denominator = denominator;
 
 	}
 
+	/**
+		* Return a string representation of the Fraction.
+	**/
 	public String toString() {
 		return numerator + "/" + denominator;
 	}
