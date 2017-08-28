b51c551515ece4f58fd23ee993920eecdabe1b22
Sami Rollins <srollins@cs.usfca.edu>
2017-08-28 16:45:42 -0700

    add fraction code

--- Fraction.java
+++ Fraction.java
@@ -9,7 +9,7 @@ public class Fraction {
 	private int denominator;
 
 	/**
-		* Constructor.
+	  * Constructor.
 	**/
 	public Fraction(int numerator, int denominator) {
 		this.numerator = numerator;
@@ -18,10 +18,46 @@ public class Fraction {
 	}
 
 	/**
+		* Constructor that takes no input.
+	**/
+	public Fraction() {
+		this.numerator = 0;
+		this.denominator = 0;
+		// this(0, 0);
+	}
+
+	/**
+		* Get the numerator.
+	**/
+	public int getNumerator() {
+		//either option is OK since we only have 
+		//access to the data member numerator
+
+		// return numerator;
+		return this.numerator;
+	}
+
+	/**
+		* Get the denominator.
+	**/
+	public int getDenominator() {
+		return this.denominator;
+	}
+
+	/**
+		* Update the numerator;
+	**/
+	public void setNumerator(int newNumerator) {
+		numerator = newNumerator;
+	}
+
+	/**
 		* Return a string representation of the Fraction.
 	**/
 	public String toString() {
 		return numerator + "/" + denominator;
 	}
+
+
 
 }
--- FractionDriver.java
+++ FractionDriver.java
@@ -9,9 +9,16 @@ public class FractionDriver {
 	public static void main(String[] args) {
 
 		//declare a variable of type fraction
+		//construct a new Fraction object with
+		//numerator 1 and denominator 3
 		Fraction f1 = new Fraction(1, 3);
 		//print the fraction
 		System.out.println(f1.toString());
 
+		//declare a variable of type fraction 
+		//construct a new Fraction object 
+		//using the second constructor
+		Fraction f2 = new Fraction();
+		System.out.println(f2.toString());
 	}
 }
