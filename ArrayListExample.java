b86f589a709984abd5b69d6752f998b82f46a3a1
Sami Rollins <srollins@cs.usfca.edu>
2017-09-11 16:17:57 -0700

    adding documentation

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -1,19 +1,29 @@
 import java.util.ArrayList;
 
+/**
+	Initial example of how to use ArrayList syntax.
+**/
 public class ArrayListExample {
 
 	public static void main(String[] args) {
 
+		//declare and initialize an ArrayList of String
 		ArrayList<String> words = new ArrayList<String>();
 
+		//add some strings to the list
 		words.add("hello");
 		words.add("goodbye");
 
+		//iterate over the list using a regular for loop
+		//note use of methods size and get
+		//the first item is at index 0
 		for(int i = 0; i < words.size(); i++) {
 			String word = words.get(i);
 			System.out.println(word);
 		}
 
+		//iterate over the list using an enhanced for loop
+		//also called a foreach loop
 		for(String word: words) {
 			System.out.println(word);
 		}
--- LoopsAndStrings.java
+++ LoopsAndStrings.java
@@ -1,19 +1,24 @@
+/**
+	A class to demonstrate how to use the methods of the String class.
+**/
 public class LoopsAndStrings {
 
 	public static void main(String[] args) {
 
+		//string with four whitespace characters, including a
+		//newline and a tab
 		String s = "hello\nclass\tit's only Monday!";
+		//use the length method to determine the length of the string
 		int size = s.length();
 
 		// System.out.println("length of string - " + s + " - is " + size);
-
-		// System.out.println(s.charAt(2));
 
 		int wscount = 0;
 
 		for(int i = 0; i < size; i++) {
 			char c = s.charAt(i);
-			
+			//use the static method isWhitespace in the Character
+			//class to determine whether a whitespace has been foundx
 			if(Character.isWhitespace(c)) {
 				System.out.println("whitespace found at position: " + i);
 				wscount++;
