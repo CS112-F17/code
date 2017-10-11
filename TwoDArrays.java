afdf0d932249488646a5630163237b4dadd2d3d9
Sami Rollins <srollins@cs.usfca.edu>
2017-10-11 14:56:01 -0700

    adding two d arrays example

--- TwoDArrays.java
+++ TwoDArrays.java
@@ -1,11 +1,15 @@
 public class TwoDArrays {
 
+	/**
+		Display the contents of the array.
+	**/
 	public static void display(double[][] scores) {
 
 		//for each row
 		for(int r = 0; r < scores.length; r++) {
 
 			System.out.print("Student " + (r+1) + ": ");
+
 			//	for each column
 			for(int c = 0; c < scores[r].length; c++) {
 				//		print item	
@@ -14,26 +18,74 @@ public class TwoDArrays {
 			System.out.println();
 
 		}
-
 	}
 
-	public static void main(String[] args) {
+	/**
+		Return the highest score from any student on any
+		assignment.
+		**/
+		public static double highScore(double[][] scores) {
 
-		double[][] scores = new double[2][3];
-		
-		scores[0][0] = 98;
-		scores[0][1] = 72;
-		scores[0][2] = 88;
+			double highScore = scores[0][0];
 
-		scores[1][0] = 94;
-		scores[1][1] = 92;
-		scores[1][2] = 97;
+			for(double[] row: scores) {
 
-		display(scores);
+				for(double score: row) {
 
+					if(score > highScore) {
+						highScore = score;
+					}
+
+				}
+			}
+			return highScore;
+
+		}
+
+	/**
+		Return a one-dimensional array containing the average
+		score for each student.
+		**/
+		public static double[] average(double[][] scores) {
+
+			double[] averages = new double[scores.length];
+			
+			//for each row - r
+			for(int r = 0; r < scores.length; r++) {			
+
+				double total = 0;				
+				//	add each item in row				
+				for(int c = 0; c < scores[r].length; c++) {
+					total += scores[r][c];
+				}
+
+				//	divide by number of items in row
+				double average = total / scores[r].length;
+				//	add to averages array - at position r
+				averages[r] = average;
+			}
+
+			return averages;
+
+		}
+
+		public static void main(String[] args) {
+
+			double[][] scores = new double[2][3];
+
+			scores[0][0] = 98;
+			scores[0][1] = 72;
+			scores[0][2] = 88;
+
+			scores[1][0] = 94;
+			scores[1][1] = 92;
+			scores[1][2] = 97;
+
+			display(scores);
+
+
+
+		}
 
 
 	}
-
-
-}
